93377e00364cb6d6b25b3d3fd8417151830dcfc8
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-03-11 13:22:48 +0100

    new dependances

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import 'semantic-ui-css/semantic.min.css'
 
 export default function App({ Component, pageProps }) {
   return (
