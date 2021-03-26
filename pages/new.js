2f0343fdf25a21f21a7e0c475e89d158ef751901
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-03-26 15:19:10 +0100

    bug fixed

--- pages/new.js
+++ pages/new.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { useEffect, useState  } from 'react';
 import fetch from 'isomorphic-unfetch';
 import { Button, Form, Loader } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
@@ -13,10 +13,10 @@ const NewPunk = () => {
     if(!user) return <div><h1>You need to be connected</h1></div>
     if (error) return <div>{error.message}</div>;
     // end if
-    const [form, setForm] = useState({ name: '', intiative: 0, is_player: false, user_id: user.sub });
+    const [form, setForm] = useState({ name: '', intiative: 0, is_player: false, user_id: 'user.sub' });
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [errors, setErrors] = useState({});
-    const router = useRouter()
+    const router = useRouter();
 
     useEffect(() => {
         if (isSubmitting) {
@@ -24,6 +24,7 @@ const NewPunk = () => {
                 //createPunk();
                 alert('submit');
             } else {
+                console.log(errors)
                 setIsSubmitting(false);
             }
         }
@@ -34,20 +35,22 @@ const NewPunk = () => {
         let errs = validate();
         setErrors(errs);
         setIsSubmitting(true);
-    };
+    }
+
+
     const handleChange = (e) => {
         setForm({
             ...form,
             [e.target.name]: e.target.value
         })
-    };
+    }
 
     const validate = () => {
         let err = {};
         if (!form.name) {
             err.name = 'Name is required';
         }
-        if (!form.initative) {
+        if (!form.initiative) {
             err.name = 'Initiative is required';
         }
         if (!form.isplayer) {
@@ -65,11 +68,31 @@ const NewPunk = () => {
                     isSubmitting
                         ? <Loader active inline='centered' />
                         : <Form onSubmit={handleSubmit}>
-                            <Form.Input fluid label='Name' placeholder='Name' name='name' onChange={handleChange} />
-                            <Form.Input fluid label='Initiative' placeholder='Initiative' name='initiative' onChange={handleChange} />
-                            <Form.Input fluid label='IsPlayer' placeholder='IsPlayer' name='isplayer' onChange={handleChange} />
-                            <Button type='submit'>Create</Button>
-
+                            <Form.Input
+                                fluid
+                                error={errors.name ? { content: 'Please enter a name', pointing: 'below' } : null}
+                                label='Name'
+                                placeholder='Name'
+                                name='name'
+                                onChange={handleChange}
+                            />
+                            <Form.Input
+                                fluid
+                                error={errors.initiative ? { content: 'Please enter a number', pointing: 'below' } : null}
+                                label='Initiative'
+                                placeholder='Initiative'
+                                name='initiative'
+                                onChange={handleChange}
+                            />
+                            <Form.Input
+                                fluid
+                                error={errors.isplayer ? { content: 'Please choce', pointing: 'below' } : null}
+                                label='IsPlayer'
+                                placeholder='IsPlayer'
+                                name='isplayer'
+                                onChange={handleChange}
+                            />
+                            <Button type='submit'>Create</Button> 
                         </Form>
                 }
             </div>
@@ -77,4 +100,4 @@ const NewPunk = () => {
     )
 }
 
-export default NewPunk
+export default NewPunk;
